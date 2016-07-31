50843fbc06c3444d248b1d3fbf8c2d378b220296
MarcoQin <qyyfy2009@163.com>
2016-07-31 22:06:26 +0800

    hash table

--- src/hash_table.c
+++ src/hash_table.c
@@ -79,3 +79,62 @@ DictEntry * lookup(DictObject *d, const char *key) {
     }
     return NULL;
 }
+
+void dictresize(DictObject *d, long size) {
+}
+
+int insert_dict_by_entry(DictObject *d, const char *key, DictEntry *ep, void *value) {
+    void *old_value;
+    if (ep->value == NULL) {
+        old_value = ep->value;
+        ep->value = value;
+        free(old_value);
+    } else {
+        if (ep->key == NULL) {
+            d->ma_fill++;
+        } else {
+            if (ep->key != dummy) {
+                log_err("Error! ep->key != dummy!");
+                exit(1);
+            }
+        }
+        ep->key = key;
+        ep->value = value;
+        d->ma_used++;
+    }
+    return 0;
+}
+
+int insert_dict(DictObject *d, const char *key, void *value) {
+    DictEntry *ep;
+    ep = lookup(d, key);
+    if (ep == NULL) {
+        free(value);
+        log_err("Fail to insert dict!");
+        return -1;
+    }
+    return insert_dict_by_entry(d, key, ep, value);
+}
+
+void dict_set_item(DictObject *d, const char *key, void *value) {
+    long n_used = d->ma_used;
+    insert_dict(d, key, value);
+    if (d->ma_used > n_used && d->ma_fill * 3 >= d->ma_size * 2)
+        dictresize(d, (d->ma_used > 50000 ? 2 : 4) * d->ma_used);
+}
+
+
+int dict_del_item(DictObject *d, const char *key) {
+    DictEntry *ep;
+    void *old_value;
+    ep = lookup(d, key);
+    if (ep == NULL)
+        return -1;
+    if (ep->value == NULL)
+        return -1;
+    old_value = ep->value;
+    ep->value = NULL;
+    ep->key = dummy;
+    d->ma_used--;
+    return 0;
+}
