72fa1d008f53c863fb99453ec7f1181d75f60962
MarcoQin <qyyfy2009@163.com>
2016-07-31 10:48:06 +0800

    update

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,4 +1,8 @@
 #include "hash_table.h"
+#include "dbg.h"
+#define MIN_DICT_SIZE = 10
+
+const char *dummy = "dummy";
 
 
 long hash(const char *key, int table_size) {
@@ -7,4 +11,41 @@ long hash(const char *key, int table_size) {
         hash_val = (hash_val << 5) + *key++;
     }
     return hash_val % table_size;
+}
+
+static int next_prime(int n) {
+    int i;
+    if (n % 2 == 0)
+        n++;
+    for (; ; n+=2) {
+        for (i = 3; i * i <= n; i += 2) {
+            if (n % i == 0)
+                goto count_outer;
+        }
+        return n;
+count_outer: ;
+    }
+}
+
+struct DictObject * Dict_New (int table_size) {
+    struct DictObject *dict;
+    int i;
+    if (table_size < MIN_DICT_SIZE)
+        table_size = MIN_DICT_SIZE;
+    dict = malloc(sizeof(struct _dictobject));
+    check_mem(dict);
+    dict->ma_size = next_prime(table_size);
+    dict->ma_fill = 0;
+    dict->ma_used = 0;
+
+    dict->ma_table = malloc(sizeof(DictObject) * dict->ma_size);
+    check_mem(dict->ma_table);
+
+    for(i = 0; i < dict->table_size; i++) {
+        dict->ma_table[i].key = NULL;
+        dict->ma_table[i].value = NULL;
+    }
+
+error:
+    exit(1);
 }
--- src/hash_table.h
+++ src/hash_table.h
@@ -2,4 +2,20 @@
 #define __HASH_TABLE_H
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct {
+    long me_hash;
+    void *key;
+    void *value;
+} DictEntry;
+
+typedef struct _dictobject DictObject;
+struct _dictobject {
+    long ma_fill;  /* Active + Dummy */
+    long ma_used; /* Active */
+    long ma_size; /* table size */
+
+    DictEntry *ma_table;
+};
+
 #endif /* ifndef __HASH_TABLE_H */
